03a25d1c11d797e8915aa0c6ef0ba57898402714
pupss89345 <pupss89345@gmail.com>
2015-04-17 15:56:03 +0800

    Homework-AOP

---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AopAlliance.Intercept;
+using System.Diagnostics;
+using KuasCore.Models;
+
+namespace KuasCore.Interceptors
+{
+    class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            //Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            Employee result = (Employee)invocation.Proceed();
+
+            result.Name = result.Name+"哈囉";
+            return result;
+        }
+    }
+}
